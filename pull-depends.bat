67b47abbe2851f4cf9350ec700272efc19c4c598
George Dregalo <37560650+gdregalo@users.noreply.github.com>
2018-08-30 13:22:56 -0400

    removed bat-native-ledger patch. modified project

--- pull-depends.bat
+++ pull-depends.bat
@@ -34,7 +34,6 @@ git checkout  %bat-native-anonize_commit%
 
 cd %ROOT%\bat-native-ledger
 git checkout  %bat-native-ledger_commit%
-git apply %PATCHES%\bat-native-ledger\bat-native-ledger.patch
 
 cd %ROOT%\bat-native-rapidjson
 git checkout  %bat-native-rapidjson_commit%
