47a5c72577cd3b69ce7ecfa698c3f2f6a146112e
George Dregalo <37560650+gdregalo@users.noreply.github.com>
2018-09-04 13:04:07 -0400

    referencing bat-native-ledger at f08c7b0

--- pull-depends.bat
+++ pull-depends.bat
@@ -16,7 +16,7 @@ type nul > %PULLLOCKFILE%
 
 :: Commits the solution is bound to
 set bat-native-anonize_commit=5e3e8eb137a1837a136a0d364ece01d0cdae6098
-set bat-native-ledger_commit=a1e05b396be117550f1b19c44c317e1b646baa80
+set bat-native-ledger_commit=f08c7b09a2d927fe50860923e523960f702ea492
 set bat-native-rapidjson_commit=744b43313525a047eda4f2e2e689aa88b6c596fa
 set bat-native-tweetnacl_commit=05ed8f82faa03609fe5ae0a4c2d454afbe2ff267
 set bip39wally-core-native_commit=e5aba371a56d3e41f7e80e868312446ce7bd434c 
